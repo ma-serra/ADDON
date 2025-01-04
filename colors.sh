226f427528302afb776f027898500fb90a424cfd
Miguel Cruz <71212944+MiguelCrux@users.noreply.github.com>
2025-01-04 16:13:32 -0300

    3 tput

--- colors.sh
+++ colors.sh
@@ -1,22 +1,11 @@
-# Verifica se o comando tput está disponível
-if command -v tput &> /dev/null; then
-    RED=$(tput setaf 1)
-    GREEN=$(tput setaf 2)
-    YELLOW=$(tput setaf 3)
-    BLUE=$(tput setaf 4)
-    MAGENTA=$(tput setaf 5)
-    CYAN=$(tput setaf 6)
-    RESET=$(tput sgr0)
-else
-    # Definição de cores usando códigos de escape ANSI
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    YELLOW='\033[0;33m'
-    BLUE='\033[0;34m'
-    MAGENTA='\033[0;35m'
-    CYAN='\033[0;36m'
-    RESET='\033[0m'
-fi
+# Definição de cores usando códigos de escape ANSI
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
 
 # Exemplo de uso das cores
 echo -e "${RED}Este é um texto vermelho${RESET}"
